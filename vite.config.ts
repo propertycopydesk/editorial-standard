cd24fb97822176d1df4fa521f89fa1f432ba5008
propertycopydesk <fatihcanwork@gmail.com>
2026-03-17 10:55:25 -0300

    vite.config FIX

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,9 +12,6 @@ export default defineConfig(({ mode }) => ({
   plugins: [
     react(),
     mode === "development" && componentTagger(),
-    viteStaticCopy({
-      targets: [{ src: "public/_redirects", dest: "" }],
-    }),
   ].filter(Boolean),
   resolve: {
     alias: {
